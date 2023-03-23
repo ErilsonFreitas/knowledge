aeabc4b0a6d71be54e8da3fc4416a746b7ad7b4b
ErilsonFreitas <herilsonfreitas2013@gmail.com>
2023-03-22 22:10:46 -0300

    Adicionado Autenticação

---
+++ backend/api/auth.js
@@ -0,0 +1,56 @@
+const { authSecret } = require('../.env')
+const jwt = require('jwt-simple')
+const bcrypt = require('bcrypt-nodejs')
+
+module.exports = app => {
+    const signin = async (req, res) => {
+        if (!req.body.email || !req.body.password) {
+            return res.status(400).send('Informe usuário e Senha!')
+        }
+
+        const user = await app.db('users')
+            .where({email: req.body.email })
+            .first()
+
+        if (!user) return res.status(400).send('Usuário não encontrado!')
+
+        const isMatch = bcrypt.compareSync(req.body.password, user.password)
+        if (!isMatch) return res.status(401).send('Email/Senha inválidos!')
+        
+        //pegando a data atual a partir de 1970 convertida pra segundos
+        const now = Math.floor(Date.now() / 1000)
+
+        const payload = {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            admin: user.admin,
+            iat: now, // iat Significa "issued at" == "Emitido em"
+            exp: now + (60 * 60 * 24 * 3) //validade do token de 3 dias
+
+        }
+
+        res.json({
+            ...payload,
+            token: jwt.encode(payload, authSecret)
+        })
+    }
+
+    const validateToken = async (req, res) => {
+        const userData = req.body || null
+        try {
+            if(userData) {
+                const token = jwt.decode(userData.token, authSecret)
+                if(new Date(token.exp * 1000) > new Date()) {
+                    return res.send(true)
+                }
+            }
+        } catch {
+            //problema no token
+        }
+
+        res.send(false)
+    }
+
+    return{ signin, validateToken}
+}
